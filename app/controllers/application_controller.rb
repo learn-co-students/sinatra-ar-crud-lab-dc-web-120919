2a6bea9803ac3a73e6c8e2e3856745e15a811830
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-01-07 22:14:18 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,58 @@
 
+# require_relative '../../config/environment'
+
+# class ApplicationController < Sinatra::Base
+
+#   configure do
+#     set :public_folder, 'public'
+#     set :views, 'app/views'
+    
+#    end
+
+#   get '/' do
+#     redirect to "/articles"
+#   end
+
+#   get "/articles" do
+#     @articles = Article.all 
+#     erb :index
+#   end
+#   get "/articles/new" do
+#     @article= Article.new
+#     erb :new
+#   end
+
+#   post "/articles" do
+#     @article = Article.create(params)
+#     redirect to "/articles/#{@article.id}"
+#   end
+
+#   get "/articles/:id" do
+#     @article = Article.find_by_id(params[:id])
+#     erb :show
+#   end
+
+#   get "articles/:id/edit" do
+#     @article = Article.find(params[:id])
+#     erb :edit
+#   end
+
+#   patch "/articles/:id" do
+#     @article = Article.find(params[:id])
+#     @article.title = params[:title]
+#     @article.content = params[:content]
+#     @article.save
+#     redirect to "/articles/#{@article.id}"
+#   end
+
+#   delete "/articles/:id" do
+#     @article = Article.find_by_id(params[:id])
+#     @article.delete
+#     redirect to "/articles"
+#   end
+# end
+
+  
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +63,53 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  # index
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  # new
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+  # create
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  # show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
+
 end
+  
+
